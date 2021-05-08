6fb037d93386d76681e15b5a63d013d1cebbb8ef
susamn <tech.supratim.samanta@gmail.com>
2021-05-08 18:32:53 -0400

    Some simple mono and flux tests

--- java/project-reactor/src/test/java/com/susamn/beingreactive/java/projectreactor/ProjectReactorApplicationTest.java
+++ java/project-reactor/src/test/java/com/susamn/beingreactive/java/projectreactor/ProjectReactorApplicationTest.java
@@ -1,8 +1,13 @@
 package com.susamn.beingreactive.java.projectreactor;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Supratim Samanta
@@ -15,6 +20,31 @@ public class ProjectReactorApplicationTest {
         Mono<String> stream = Mono.just("data");
         StepVerifier.create(stream)
                 .expectNext("data")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testSimpleFlux(){
+        Flux<String> stream = Flux.just("data","from","some","datasource");
+        StepVerifier.create(stream)
+                .expectSubscription() // Makes sure that there is a valid subscription, otherwise the flux will not send any data
+                .expectNext("data")
+                .expectNext("from")
+                .expectNext("some")
+                .expectNext("datasource")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testSimpleFluxFromIterable(){
+        List<String> d = new ArrayList<>(Collections.emptyList());
+        d.add("data1");
+        d.add("data2");
+        Flux<String> stream = Flux.fromIterable(d);
+        StepVerifier.create(stream)
+                .expectSubscription() // Makes sure that there is a valid subscription, otherwise the flux will not send any data
+                .expectNext("data1")
+                .expectNext("data2")
                 .verifyComplete();
     }
 }
